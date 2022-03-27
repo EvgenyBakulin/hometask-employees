4e7ca0509566f50b853678ed208749ec45e78b75
Евгений <ugend84@yandex.ru>
2022-03-27 13:41:07 +0300

    Initial commit

---
+++ src/main/java/pro/sky/hometaskemployees/HometaskEmployeesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.hometaskemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometaskEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HometaskEmployeesApplication.class, args);
+	}
+
+}
