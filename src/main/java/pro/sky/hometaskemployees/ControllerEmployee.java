71d049228200dccf76643945361475f477b69b79
Евгений <ugend84@yandex.ru>
2022-03-27 13:51:28 +0300

    Коммит пробный

---
+++ src/main/java/pro/sky/hometaskemployees/ControllerEmployee.java
@@ -0,0 +1,10 @@
+package pro.sky.hometaskemployees;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerEmployee {
+    public String hello() {
+        return "hello";
+    }
+}
